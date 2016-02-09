d59f88c3551f4bc9cbec2d3325d7a332e19dc52b
Jack Lynch <jacklynch@wharnal.com>
2016-02-08 20:13:53 -0500

    barebones bot

---
+++ bot.py
@@ -0,0 +1,22 @@
+import discord
+import asyncio
+import configparser
+
+config = configparser.ConfigParser()
+config.read('settings.cfg')
+user = config['credentials']['user']
+password = config['credentials']['user']
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(client.user.name)
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('meme'):
+        await client.send_message(message.channel, 'nice meme!')
+
+
+client.run(user, password)
